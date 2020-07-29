81a045ac83a95b3f698f898fe09ce9edd76d3063
catonrug <aigars.kadikis@gmail.com>
2020-07-29 17:14:19 +0300

    next

---
+++ zabbix_backup_postgres.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+year=$(date +%Y)
+month=$(date +%m)
+day=$(date +%d)
+clock=$(date +%H%M)
+volume=/backup/postgres
+dest=$volume/$year/$month/$day/$clock
+
+if [ ! -d "$dest" ]; then
+  mkdir -p "$dest"
+fi
+
+for db in $(
+PGPORT=7412 PGPASSWORD=zabbix PGUSER=postgres psql -h 10.133.112.87 -t -A -c "SELECT datname FROM pg_database where datname not in ('template0','template1','postgres','dummy_db')"
+) ; do echo $db; PGPORT=7412 PGPASSWORD=zabbix PGUSER=postgres pg_dump -h 10.133.112.87 $db | gzip --best > $dest/$db.sql.gz ; done
+
+rclone -vv sync $volume BackupPostgreSQL:postgres
