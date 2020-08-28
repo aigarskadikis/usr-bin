de43f7b41c1ea2aba4289da77c54caad00b002f2
catonrug <aigars.kadikis@gmail.com>
2020-08-28 17:32:12 +0300

    next

--- zabbix_backup_sleep.sh
+++ zabbix_backup_sleep.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 SLEEP=10
+DB=zabbix
 
+echo "backuping schema"
 mysqldump \
 --flush-logs \
 --single-transaction \
@@ -10,17 +12,18 @@ zabbix > schema.sql
 echo "sleeping for $SLEEP seconds"
 sleep $SLEEP
 
+echo "backuping data without history and trends"
 mysqldump \
 --flush-logs \
 --single-transaction \
 --no-create-info \
---ignore-table=history \
---ignore-table=history_log \
---ignore-table=history_str \
---ignore-table=history_text \
---ignore-table=history_uint \
---ignore-table=trends \
---ignore-table=trends_uint \
+--ignore-table=$DB.history \
+--ignore-table=$DB.history_log \
+--ignore-table=$DB.history_str \
+--ignore-table=$DB.history_text \
+--ignore-table=$DB.history_uint \
+--ignore-table=$DB.trends \
+--ignore-table=$DB.trends_uint \
 zabbix > data.sql
 sleep $SLEEP
 
@@ -32,9 +35,9 @@ history_text
 history_log
 trends
 trends_uint
-" | grep -v "^$" | while IFS= read -r table; do {
-echo $table
-mysql --flush-logs --single-transaction --no-create-info zabbix $table > $table.sql
+" | grep -v "^$" | while IFS= read -r TABLE; do {
+echo "backuping $TABLE"
+mysql --flush-logs --single-transaction --no-create-info $DB $TABLE > $TABLE.sql
 echo "sleeping for $SLEEP seconds"
 sleep $SLEEP
 } done
