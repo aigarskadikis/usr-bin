5639b5b8da0ba8d001003ec02643e91e8c37f540
catonrug <aigars.kadikis@gmail.com>
2020-09-26 00:53:38 +0300

    next

--- zabbix_mysql_db_maintenance.sh
+++ zabbix_mysql_db_maintenance.sh
@@ -19,8 +19,8 @@ OLD=$(echo $TABLE|sed "s|$|_old|")
 TMP=$(echo $TABLE|sed "s|$|_tmp|")
 
 # do not distract environment while optimizing
-echo "RENAME TABLE $TABLE TO $OLD;"
-echo "CREATE TABLE $TABLE LIKE $OLD;"
+mysql $DB -e "RENAME TABLE $TABLE TO $OLD"
+mysql $DB -e "CREATE TABLE $TABLE LIKE $OLD"
 
 PART_LIST=$(
 mysql $DB -e " \
@@ -29,17 +29,18 @@ SHOW CREATE TABLE $TABLE\G
 grep -oP 'PARTITION \Kp[0-9_]+')
 if [ -z "$PART_LIST" ] 
 then
-echo "OPTIMIZE TABLE $OLD;"
+mysql $DB -e "OPTIMIZE TABLE $OLD"
 else
 PARTITIONS=$(echo "$PART_LIST" | sed "s|$|,|" | tr -cd "[:print:]" | sed "s|.$||")
-echo "ALTER TABLE $OLD REORGANIZE PARTITION $PARTITIONS"
+mysql $DB -e "ALTER TABLE $OLD REORGANIZE PARTITION $PARTITIONS"
 fi
 
-echo "RENAME TABLE $TABLE TO $TMP; RENAME TABLE $OLD TO $TABLE;"
-echo "INSERT IGNORE INTO $TABLE SELECT * FROM $TMP;"
-echo "DROP TABLE $TMP;"
+mysql $DB -e "RENAME TABLE $TABLE TO $TMP; RENAME TABLE $OLD TO $TABLE"
+mysql $DB -e "SET SESSION SQL_LOG_BIN=0; INSERT IGNORE INTO $TABLE SELECT * FROM $TMP"
+mysql $DB -e "DROP TABLE $TMP"
 echo
 
 } done
 
 /usr/sbin/zabbix_server -R housekeeper_execute
+
