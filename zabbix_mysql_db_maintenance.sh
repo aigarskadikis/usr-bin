022848028c5750e6c01ad2624998dc31a6237b44
catonrug <aigars.kadikis@gmail.com>
2020-09-26 01:38:30 +0300

    next

--- zabbix_mysql_db_maintenance.sh
+++ zabbix_mysql_db_maintenance.sh
@@ -3,6 +3,7 @@ SLEEP=1
 DB=zabbix
 
 echo "
+history_str
 history_log
 " | 
 grep -v "^$" | \
@@ -30,25 +31,31 @@ echo "OPTIMIZE TABLE $OLD;"
 mysql $DB -e "OPTIMIZE TABLE $OLD;"
 
 else
-PARTITIONS=$(echo "$PART_LIST" | sed "s|$|,|" | tr -cd "[:print:]" | sed "s|.$||")
 
-echo "ALTER TABLE $OLD REBUILD PARTITION $PARTITIONS;"
-mysql $DB -e "ALTER TABLE $OLD REORGANIZE PARTITION $PARTITIONS;"
+echo "$PART_LIST" | \
+grep -v "^$" | \
+while IFS= read -r PARTITION
+do {
+
+echo "ALTER TABLE $OLD REBUILD PARTITION $PARTITION;"
+mysql $DB -e "ALTER TABLE $OLD REBUILD PARTITION $PARTITION;"
+
+} done
 
 fi
 
 echo "RENAME TABLE $TABLE TO $TMP; RENAME TABLE $OLD TO $TABLE;"
 mysql $DB -e "RENAME TABLE $TABLE TO $TMP; RENAME TABLE $OLD TO $TABLE;"
 
-echo "SET SESSION SQL_LOG_BIN=0; INSERT IGNORE INTO $TABLE SELECT * FROM $TMP"
+echo "SET SESSION SQL_LOG_BIN=0; INSERT IGNORE INTO $TABLE SELECT * FROM $TMP;"
 mysql $DB -e "SET SESSION SQL_LOG_BIN=0; INSERT IGNORE INTO $TABLE SELECT * FROM $TMP;"
 
-#echo "DROP TABLE $TMP"
-#mysql $DB -e "DROP TABLE $TMP"
+echo "DROP TABLE $TMP;"
+mysql $DB -e "DROP TABLE $TMP;"
 
 echo
 
 } done
 
-/usr/sbin/zabbix_server -R housekeeper_execute
+#/usr/sbin/zabbix_server -R housekeeper_execute
 
