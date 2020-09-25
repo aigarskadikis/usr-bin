bb75959d7825dcfc7b898494cab28227c548fabd
catonrug <aigars.kadikis@gmail.com>
2020-09-26 00:47:00 +0300

    next

--- zabbix_mysql_db_maintenance.sh
+++ zabbix_mysql_db_maintenance.sh
@@ -14,7 +14,14 @@ trends_uint
 grep -v "^$" | \
 while IFS= read -r TABLE
 do {
-#echo table $TABLE
+
+OLD=$(echo $TABLE|sed "s|$|_old|")
+TMP=$(echo $TABLE|sed "s|$|_tmp|")
+
+# do not distract environment while optimizing
+echo "RENAME TABLE $TABLE TO $OLD;"
+echo "CREATE TABLE $TABLE LIKE $OLD;"
+
 PART_LIST=$(
 mysql $DB -e " \
 SHOW CREATE TABLE $TABLE\G
@@ -22,9 +29,16 @@ SHOW CREATE TABLE $TABLE\G
 grep -oP 'PARTITION \Kp[0-9_]+')
 if [ -z "$PART_LIST" ] 
 then
-echo OPTIMIZE TABLE $TABLE;
+echo "OPTIMIZE TABLE $OLD;"
 else
-echo ALTER TABLE $TABLE REORGANIZE PARTITION $(echo "$PART_LIST" | sed "s|$|,|" | tr -cd '[:print:]' | sed "s|.$||");
+PARTITIONS=$(echo "$PART_LIST" | sed "s|$|,|" | tr -cd "[:print:]" | sed "s|.$||")
+echo "ALTER TABLE $OLD REORGANIZE PARTITION $PARTITIONS"
 fi
+
+echo "RENAME TABLE $TABLE TO $TMP; RENAME TABLE $OLD TO $TABLE;"
+echo "INSERT IGNORE INTO $TABLE SELECT * FROM $TMP;"
+echo "DROP TABLE $TMP;"
+echo
+
 } done
 
