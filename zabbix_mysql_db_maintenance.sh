a7662c7d0ee761c396c55508ae2e6ce7105ef9ec
catonrug <aigars.kadikis@gmail.com>
2020-09-26 01:21:23 +0300

    next

--- zabbix_mysql_db_maintenance.sh
+++ zabbix_mysql_db_maintenance.sh
@@ -3,13 +3,7 @@ SLEEP=1
 DB=zabbix
 
 echo "
-history
-history_uint
-history_str
-history_text
 history_log
-trends
-trends_uint
 " | 
 grep -v "^$" | \
 while IFS= read -r TABLE
@@ -19,10 +13,10 @@ OLD=$(echo $TABLE|sed "s|$|_old|")
 TMP=$(echo $TABLE|sed "s|$|_tmp|")
 
 # do not distract environment while optimizing
-echo "RENAME TABLE $TABLE TO $OLD"
-#mysql $DB -e "RENAME TABLE $TABLE TO $OLD"
-echo "CREATE TABLE $TABLE LIKE $OLD"
-#mysql $DB -e "CREATE TABLE $TABLE LIKE $OLD"
+echo "RENAME TABLE $TABLE TO $OLD;"
+mysql $DB -e "RENAME TABLE $TABLE TO $OLD;"
+echo "CREATE TABLE $TABLE LIKE $OLD;"
+mysql $DB -e "CREATE TABLE $TABLE LIKE $OLD;"
 
 PART_LIST=$(
 mysql $DB -e " \
@@ -32,24 +26,24 @@ grep -oP 'PARTITION \Kp[0-9_]+')
 if [ -z "$PART_LIST" ] 
 then
 
-echo "OPTIMIZE TABLE $OLD"
-#mysql $DB -e "OPTIMIZE TABLE $OLD"
+echo "OPTIMIZE TABLE $OLD;"
+mysql $DB -e "OPTIMIZE TABLE $OLD;"
 
 else
 PARTITIONS=$(echo "$PART_LIST" | sed "s|$|,|" | tr -cd "[:print:]" | sed "s|.$||")
 
-echo "ALTER TABLE $OLD REORGANIZE PARTITION $PARTITIONS"
-#mysql $DB -e "ALTER TABLE $OLD REORGANIZE PARTITION $PARTITIONS"
+echo "ALTER TABLE $OLD REBUILD PARTITION $PARTITIONS;"
+mysql $DB -e "ALTER TABLE $OLD REORGANIZE PARTITION $PARTITIONS;"
 
 fi
 
-echo "RENAME TABLE $TABLE TO $TMP; RENAME TABLE $OLD TO $TABLE"
-#mysql $DB -e "RENAME TABLE $TABLE TO $TMP; RENAME TABLE $OLD TO $TABLE"
+echo "RENAME TABLE $TABLE TO $TMP; RENAME TABLE $OLD TO $TABLE;"
+mysql $DB -e "RENAME TABLE $TABLE TO $TMP; RENAME TABLE $OLD TO $TABLE;"
 
 echo "SET SESSION SQL_LOG_BIN=0; INSERT IGNORE INTO $TABLE SELECT * FROM $TMP"
-#mysql $DB -e "SET SESSION SQL_LOG_BIN=0; INSERT IGNORE INTO $TABLE SELECT * FROM $TMP"
+mysql $DB -e "SET SESSION SQL_LOG_BIN=0; INSERT IGNORE INTO $TABLE SELECT * FROM $TMP;"
 
-echo "DROP TABLE $TMP"
+#echo "DROP TABLE $TMP"
 #mysql $DB -e "DROP TABLE $TMP"
 
 echo
