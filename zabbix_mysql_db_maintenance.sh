62c06fc8b72b89f990459854f8491fe84d3e8be7
catonrug <aigars.kadikis@gmail.com>
2020-09-25 19:23:16 +0300

    next

---
+++ zabbix_mysql_db_maintenance.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+SLEEP=1
+DB=zabbix
+
+echo "
+history
+history_uint
+history_str
+history_text
+history_log
+trends
+trends_uint
+" | 
+grep -v "^$" | \
+while IFS= read -r TABLE
+do {
+mysql $DB -e "SHOW CREATE TABLE $TABLE\G"
+} done
+
