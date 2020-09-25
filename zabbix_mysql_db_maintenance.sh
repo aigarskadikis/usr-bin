4f5ad2204d999e8b9d5234d985e9f8cbd63deb71
catonrug <aigars.kadikis@gmail.com>
2020-09-26 00:28:31 +0300

    next

--- zabbix_mysql_db_maintenance.sh
+++ zabbix_mysql_db_maintenance.sh
@@ -14,9 +14,17 @@ trends_uint
 grep -v "^$" | \
 while IFS= read -r TABLE
 do {
+#echo table $TABLE
+PART_LIST=$(
 mysql $DB -e " \
 SHOW CREATE TABLE $TABLE\G
 " | \
-grep -oP 'PARTITION \Kp[0-9_]+'
+grep -oP 'PARTITION \Kp[0-9_]+')
+if [ -z "$PART_LIST" ] 
+then
+echo OPTIMIZE TABLE $TABLE;
+else
+echo ALTER TABLE $TABLE REORGANIZE PARTITION $(echo "$PART_LIST" | sed "s|$|,|" | tr -cd '[:print:]' | sed "s|.$||");
+fi
 } done
 
