f62a67dd8e4bdf353dd49d4a54774af2c21266a5
catonrug <aigars.kadikis@gmail.com>
2020-09-27 13:50:31 +0300

    next

--- zabbix_partitioning.sh
+++ zabbix_partitioning.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 # (auth is in ~/.my.cnf)
 
-# size before pruning
-df /var/lib/mysql
-
+# define partition maintenance command
 command="CALL partition_maintenance_all('zabbix');"
 
 # perform partition maintenance to drop all rolloff data
 mysql zabbix -e "$command"
-
-# size after pruning
-df /var/lib/mysql
 
