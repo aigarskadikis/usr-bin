858766ae179f888e03d5646a5375fb711adf6840
catonrug <aigars.kadikis@gmail.com>
2022-04-27 09:55:34 +0300

    next

--- zabbix_backup_postgres.sh
+++ zabbix_backup_postgres.sh
@@ -15,4 +15,16 @@ for db in $(
 PGPORT=7413 PGPASSWORD=postgres PGUSER=postgres psql -h 158.101.218.248 -t -A -c "SELECT datname FROM pg_database where datname not in ('template0','template1','postgres','dummy_db')"
 ) ; do echo $db; PGPORT=7413 PGPASSWORD=postgres PGUSER=postgres pg_dump -h 158.101.218.248 $db | xz > $dest/$db.sql.xz ; done
 
+# remove older files than 30 days
+echo -e "\nThese files will be deleted:"
+find /backup/postgres -type f -mtime +15
+# delete files
+find /backup/postgres -type f -mtime +15 -delete
+
+echo -e "\nRemoving empty directories:"
+find /backup/postgres -type d -empty -print
+# delete empty directories
+find /backup/postgres -type d -empty -print -delete
+
 rclone -vv sync $volume BackupPostgreSQL:postgres
+
